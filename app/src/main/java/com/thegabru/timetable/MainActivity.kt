c6f52297ac1e032f85e6b3d4fd4e35c0990501fa
Digvijay Singh <digvijays386@gmail.com>
2018-08-22 11:44:31 +0530

    added second activity

--- app/src/main/java/com/thegabru/timetable/MainActivity.kt
+++ app/src/main/java/com/thegabru/timetable/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.thegabru.timetable
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button.setOnClickListener {
+            val msg = edit.text.toString()
+            Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
